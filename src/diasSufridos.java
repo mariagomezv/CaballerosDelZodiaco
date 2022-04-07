74d126a12424a9803de10f3847ada3760192664b
RdzJJ <98420262+RdzJJ@users.noreply.github.com>
2022-04-07 11:40:13 -0500

    modified class diasSufridos.java

--- src/diasSufridos.java
+++ src/diasSufridos.java
@@ -1,15 +1,19 @@
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
-import java.util.Scanner;
 public class diasSufridos {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
+    private int diaN;
+    private int mesN;
+    private int anioN;
+
+    public diasSufridos() {
+        /*Scanner scanner = new Scanner(System.in);
         System.out.println("Digite su dia de nacimiento");
         int diaN = scanner.nextInt();
         System.out.println("Digite su mes de nacimiento");
         int mesN = scanner.nextInt();
         System.out.println("Digite su año de nacimiento");
-        int anioN = scanner.nextInt();
+        int anioN = scanner.nextInt();*/
         LocalDate diaNacimiento = LocalDate.of(anioN, mesN, diaN);
         LocalDate fechaActual = LocalDate.now();
         long diasSufridos= ChronoUnit.DAYS.between(diaNacimiento, fechaActual);
