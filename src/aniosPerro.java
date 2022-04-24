98bfbbe5ff74a5adf4ce8e086f2f89bc7785f426
mariagomezv <97989023+mariagomezv@users.noreply.github.com>
2022-04-24 11:56:31 -0500

    fix some errors on aniosPerro

--- src/aniosPerro.java
+++ src/aniosPerro.java
@@ -1,8 +1,17 @@
-public class aniosPerro(){
-    public void aniosPerro() {
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class aniosPerro{
+    private int diaN;
+    private int mesN;
+    private int anioN;
+    private long diasConver;
+    public void calAniosPerro() {
+        
         LocalDate diaNaci = LocalDate.of(anioN, mesN, diaN);
         LocalDate fechaAct = LocalDate.now();
         diasConver = ChronoUnit.DAYS.between(diaNaci, fechaAct);
         System.out.println((diasConver / 365) / 7);
-    }    
+    }
+
 }
